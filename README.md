26b59114a3d6270c9b67653117718462d6f3cc33
Alberto <malberto24@icloud.com>
2020-09-29 20:38:32 -0500

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Count-Vowels
+# count-vowels
+
+Simple application that returns a list of vowels in a text. 
+
+Live Preview: https://albeey.github.io/Count-Vowels/
---
+++ scripts/script.js
@@ -0,0 +1,76 @@
+let vowelCount = {a: 0, 
+                  e: 0, 
+                  i: 0, 
+                  o: 0, 
+                  u: 0}
+
+let alphabeticCharCount = 0; 
+let totalVowelCount = 0;           
+
+function countVowels(textInput) {
+    let toArr = textInput.replace(/[^a-zA-Z]+/g, "").split("");
+    alphabeticCharCount = toArr.length;
+    for (let i = 0; i < toArr.length; i++) {
+        switch (toArr[i]) {
+            case "a":
+                vowelCount.a++;
+                totalVowelCount++;
+                break;
+            case "e":
+                vowelCount.e++;
+                totalVowelCount++;
+                break;
+            case "i":
+                vowelCount.i++;
+                totalVowelCount++;
+                break;      
+            case "o":
+                vowelCount.o++;
+                totalVowelCount++;
+                break;    
+            case "u":
+                vowelCount.u++;
+                totalVowelCount++;
+                break;                                     
+        }
+    }
+}
+
+const countBttn = document.querySelector("#countBttn");
+const clearBttn = document.querySelector("#clearBttn");
+const inputBox = document.querySelector("#inputBox");
+
+let resultArea = document.querySelector(".result-area");
+let textContent = "";
+
+countBttn.addEventListener("click", () => {
+    textContent = inputBox.value;
+    showAnswer(countVowels(textContent));
+})
+
+clearBttn.addEventListener("click", () => {
+    inputBox.value = textContent;
+    resultArea.style.display = "none";
+})
+
+function showAnswer(answer) {
+    if (textContent.trim().length) resultArea.style.display = "block";
+    resultArea.innerHTML = `Your text has <span class="number">${alphabeticCharCount}</span> alphabetic characters, <span class="number">${totalVowelCount}</span> are vowels.<br>
+                            a: <span class="number">${vowelCount.a}</span><br>
+                            e: <span class="number">${vowelCount.e}</span><br>
+                            i: <span class="number">${vowelCount.i}</span><br>
+                            o: <span class="number">${vowelCount.o}</span><br>
+                            u: <span class="number">${vowelCount.u}</span>`;
+    resetValues();
+}
+
+function resetValues() {
+    alphabeticCharCount = 0; 
+    totalVowelCount = 0;   
+
+    for (const [key] of Object.entries(vowelCount)) {
+        vowelCount[key] = 0;
+    }
+
+    textContent = "";
+}
