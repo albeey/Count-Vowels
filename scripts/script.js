e8a54122f357efe3759a31b6a237e9142bb92fcc
Alberto <malberto24@icloud.com>
2020-09-29 22:13:31 -0500

    scripts/script.js

--- scripts/script.js
+++ scripts/script.js
@@ -54,13 +54,20 @@ clearBttn.addEventListener("click", () => {
 })
 
 function showAnswer(answer) {
-    if (textContent.trim().length) resultArea.style.display = "block";
-    resultArea.innerHTML = `Your text has <span class="number">${alphabeticCharCount}</span> alphabetic characters, <span class="number">${totalVowelCount}</span> are vowels.<br>
-                            a: <span class="number">${vowelCount.a}</span><br>
-                            e: <span class="number">${vowelCount.e}</span><br>
-                            i: <span class="number">${vowelCount.i}</span><br>
-                            o: <span class="number">${vowelCount.o}</span><br>
-                            u: <span class="number">${vowelCount.u}</span>`;
+    if (!textContent.trim().length) {}
+    else if (alphabeticCharCount === 0) {
+        resultArea.style.display = "block";
+        resultArea.innerHTML = `<center>Please enter at least one alphabetic character.</center>`;
+    } else if(textContent.trim().length) {
+        resultArea.style.display = "block";
+        resultArea.innerHTML = `Your text has <span class="number">${alphabeticCharCount}</span> 
+                                alphabetic characters, <span class="number">${totalVowelCount}</span> are vowels.<br>
+                                a: <span class="number">${vowelCount.a}</span><br>
+                                e: <span class="number">${vowelCount.e}</span><br>
+                                i: <span class="number">${vowelCount.i}</span><br>
+                                o: <span class="number">${vowelCount.o}</span><br>
+                                u: <span class="number">${vowelCount.u}</span>`;
+    }
     resetValues();
 }
 
